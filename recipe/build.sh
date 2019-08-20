7b1e0e0bb22a502d5f35e01888a84309e74601a8
Pierre C <pierrec@deepmarimba.com>
2019-08-20 17:25:05 -0400

    Re-export toolchain tools without build-prefix

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,20 @@
 set -e
 set -x
 
+export CC=$(basename $CC)
+export CPP=$(basename $CPP)
+export CXX=$(basename $CXX)
+export STRIP=$(basename $STRIP)
+export NM=$(basename $NM)
+export OBJDUMP=$(basename $OBJDUMP)
+export AS=$(basename $AS)
+export AR=$(basename $AR)
+export RANLIB=$(basename $RANLIB)
+export LD=$(basename $LD)
+
 ./configure --prefix="$PREFIX" --disable-install-doc --enable-load-relative \
   --enable-shared --with-openssl-dir="$PREFIX" --with-readline-dir="$PREFIX" \
   --with-tcl-dir="$PREFIX" --with-tk-dir="$PREFIX" --with-libyaml-dir="$PREFIX" \
   --with-zlib-dir="$PREFIX"
 make -j ${CPU_COUNT}
 make install
-
-rbconfig=`find $PREFIX -type f -name 'rbconfig.rb'`
-sed -f $RECIPE_DIR/rbconfig.sed -i $rbconfig
