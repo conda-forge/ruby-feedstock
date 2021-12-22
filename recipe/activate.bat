9618864c93193ce8dbd5cbedb7121b1898bb9c80
Arnold Bloemert <arnold.bloemert@nl.bosch.com>
2021-12-22 12:50:37 +0100

    Fix activate script for Windows

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -1,3 +1,3 @@
 @echo off
-set GEM_HOME=%CONDA_PREFIX%\Library\share\rubygems
-set PATH=%CONDA_PREFIX%\Library\share\rubygems\bin;%PATH%
+set GEM_HOME=%CONDA_PREFIX%\share\rubygems
+set PATH=%CONDA_PREFIX%\share\rubygems\bin;%PATH%
