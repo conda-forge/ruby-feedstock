e0e36f182e2245caad72fa1998cf19dd8e37cba2
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-06-27 20:01:37 +0200

    make verbose

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,51 +1,21 @@
 #!/bin/bash
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./tool
 set -e
 set -x
 
-export CC=$(basename $CC)
-export CPP=$(basename $CPP)
-export CXX=$(basename $CXX)
-export STRIP=$(basename $STRIP)
-export NM=$(basename $NM)
-export OBJDUMP=$(basename $OBJDUMP)
-export AS=$(basename $AS)
-export AR=$(basename $AR)
-export RANLIB=$(basename $RANLIB)
-export LD=$(basename $LD)
-
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
-  (
-    mkdir -p build-host
-    pushd build-host
-
-    export CC=$CC_FOR_BUILD
-    export CXX=$CXX_FOR_BUILD
-    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
-
-    # Unset them as we're ok with builds that are either slow or non-portable
-    unset CFLAGS
-    unset CXXFLAGS
-
-    # --enable-shared \
-    ../configure \
-      --host=$BUILD \
-      --prefix="$BUILD_PREFIX" \
-      --disable-install-doc \
-      --enable-load-relative \
-      --with-libffi-dir="$BUILD_PREFIX" \
-      --with-libyaml-dir="$BUILD_PREFIX" \
-      --with-openssl-dir="$BUILD_PREFIX" \
-      --with-readline-dir="$BUILD_PREFIX" \
-      --with-zlib-dir="$BUILD_PREFIX"
-
-    make -j ${CPU_COUNT}
-    make install
-  )
-fi
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./tool
 
 autoconf
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
+  if [[ $target_platform == "linux-aarch64" ]]; then
+    CARGO_BUILD_TARGET="aarch64-unknown-linux-gnu"
+  elif [[ $target_platform == "linux-ppc64le" ]]; then
+    CARGO_BUILD_TARGET="powerpc64le-unknown-linux-gnu"
+  elif [[ $target_platform == "osx-arm64" ]]; then
+    CARGO_BUILD_TARGET="aarch64-apple-darwin"
+  fi
+fi
 
 ./configure \
   --prefix="${PREFIX}" \
@@ -58,7 +28,7 @@ autoconf
   --with-readline-dir="$PREFIX" \
   --with-zlib-dir="$PREFIX"
 
-make -j ${CPU_COUNT}
+make VERBOSE=1 -j ${CPU_COUNT}
 # make check works locally on Linux, but not on CI Nodes, issue seems related to IPv6 and closed ports
 # make check
 make install
