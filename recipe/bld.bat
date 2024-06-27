8faf8d693f891ec4fe3d0f640e5065499280713d
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-06-27 22:12:13 +0200

    ..

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,11 +9,15 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 nmake install
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-mkdir %PREFIX%\etc
-if %errorlevel% neq 0 exit /b %errorlevel%
+if not exist "%PREFIX%\etc" (
+    mkdir "%PREFIX%\etc"
+    if %errorlevel% neq 0 exit /b %errorlevel%
+)
 
-mkdir %PREFIX%\share\rubygems
-if %errorlevel% neq 0 exit /b %errorlevel%
+if not exist "%PREFIX%\share\rubygems" (
+    mkdir "%PREFIX%\share\rubygems"
+    if %errorlevel% neq 0 exit /b %errorlevel%
+)
 
 echo "gemhome: %PREFIX%/share/rubygems" > %PREFIX%/etc/gemrc
 if %errorlevel% neq 0 exit /b %errorlevel%
